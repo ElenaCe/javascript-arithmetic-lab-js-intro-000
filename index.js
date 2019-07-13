fc63acdea857a6abe038d7ecdf6168285d66da0a
Elena Cellitti <elenacellitti92@gmail.com>
2019-07-13 15:58:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ function inc(n){
 function dec(n){
  return  n -= 1
 }
+
+function makeInt(n){
+  
+}
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+  })
